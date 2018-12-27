da6a76e62b1aead17f51ca84ee91df3627d35a84
Jose Nunes <josenunes34@gmail.com>
2018-12-27 16:30:24 +0000

    Static form

--- src/components/ContactForm.js
+++ src/components/ContactForm.js
@@ -55,139 +55,39 @@ const Button = styled.button`
   }
 `;
 
-const Success = styled.div`
-  background: ${props => props.theme.success100};
-  border: 1px solid ${props => props.theme.success500};
-  border-radius: 4px;
-  color: ${props => props.theme.success500};
-  padding: 8px;
-`;
-
-const Error = styled.div`
-  background: ${props => props.theme.error100};
-  border: 1px solid ${props => props.theme.error500};
-  border-radius: 4px;
-  color: ${props => props.theme.error500};
-  padding: 8px;
-`;
-
-const encode = data => {
-  return Object.keys(data)
-    .map(key => encodeURIComponent(key) + '=' + encodeURIComponent(data[key]))
-    .join('&');
-};
-
 class ContactForm extends Component {
-  initialState = {
-    submitted: false,
-    error: '',
-    firstName: '',
-    lastName: '',
-    email: '',
-    subject: '',
-    message: '',
-  };
-
-  state = this.initialState;
-
-  handleChange = e => {
-    this.setState({ [e.target.name]: e.target.value, submitted: false, error: false });
-  };
-
-  handleSubmit = e => {
-    e.preventDefault();
-    const form = e.target;
-    fetch('/', {
-      method: 'POST',
-      headers: { 'Content-Type': 'application/x-www-form-urlencoded' },
-      body: encode({
-        'form-name': form.getAttribute('name'),
-        'bot-field': this.state['bot-field'],
-        firstName: this.state.firstName,
-        lastName: this.state.lastName,
-        email: this.state.email,
-        subject: this.state.subject,
-        message: this.state.message,
-      }),
-    })
-      .then(() => this.setState({ ...this.initialState, submitted: true }))
-      .catch(() => this.setState({ ...this.initialState, error: true }));
-  };
-
   render() {
-    const { submitted, error } = this.state;
     return (
       <StyledForm
         name="contact"
         method="POST"
         data-netlify="true"
         data-netlify-honeypot="bot-field"
-        onSubmit={this.handleSubmit}
       >
         <input type="hidden" name="form-name" value="contact" />
         <input type="hidden" name="bot-field" />
         <div className="name-row">
           <div className="input-group">
             <label>First name:</label>
-            <input
-              type="text"
-              name="firstName"
-              value={this.state.firstName}
-              onChange={this.handleChange}
-              required
-            />
+            <input type="text" name="firstName" required />
           </div>
           <div className="input-group">
             <label>Last name:</label>
-            <input
-              type="text"
-              name="lastName"
-              value={this.state.lastName}
-              onChange={this.handleChange}
-              required
-            />
+            <input type="text" name="lastName" required />
           </div>
         </div>
         <div className="input-group">
           <label>Email:</label>
-          <input
-            type="email"
-            name="email"
-            value={this.state.email}
-            onChange={this.handleChange}
-            required
-          />
+          <input type="email" name="email" required />
         </div>
         <div className="input-group">
           <label>Subject:</label>
-          <input
-            type="text"
-            name="subject"
-            value={this.state.subject}
-            onChange={this.handleChange}
-            required
-          />
+          <input type="text" name="subject" required />
         </div>
         <div className="input-group">
           <label>Message:</label>
-          <textarea
-            rows="6"
-            name="message"
-            value={this.state.message}
-            onChange={this.handleChange}
-            required
-          />
+          <textarea rows="6" name="message" required />
         </div>
-        {submitted && (
-          <Success>
-            Thank you, your message was sent successfully. I will get in touch soon.
-          </Success>
-        )}
-        {error && (
-          <Error>
-            Something went wrong. Please, try again or email me at mceuteixeira1@gmail.com.
-          </Error>
-        )}
         <div className="button-container">
           <Button type="submit">Send message</Button>
         </div>
