8dab0d9ca4263606cea9ed18cc490da19f737f33
Jose Nunes <josenunes34@gmail.com>
2018-12-27 14:34:07 +0000

    hotfix---Fix form submission

--- src/components/ContactForm.js
+++ src/components/ContactForm.js
@@ -55,29 +55,70 @@ const Button = styled.button`
   }
 `;
 
+const encode = data => {
+  return Object.keys(data)
+    .map(key => encodeURIComponent(key) + '=' + encodeURIComponent(data[key]))
+    .join('&');
+};
+
 class ContactForm extends Component {
+  state = { submitted: false };
+
+  handleChange = e => {
+    this.setState({ [e.target.name]: e.target.value });
+  };
+
+  handleSubmit = e => {
+    e.preventDefault();
+    const form = e.target;
+    fetch('/', {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/x-www-form-urlencoded' },
+      body: encode({
+        'form-name': form.getAttribute('name'),
+        ...this.state,
+      }),
+    })
+      .then(() => this.setState({ submitted: true }))
+      .catch(error => alert(error));
+  };
+
   render() {
-    return (
-      <StyledForm name="contact" method="POST" data-netlify="true" netlify-honeypot="bot-field">
+    const { submitted } = this.state;
+    return submitted ? (
+      <div>Thank you :)</div>
+    ) : (
+      <StyledForm
+        name="contact"
+        method="POST"
+        data-netlify="true"
+        netlify-honeypot="bot-field"
+        onSubmit={this.handleSubmit}
+      >
+        <input type="hidden" name="form-name" value="contact" />
+        <input type="hidden" name="bot-field" />
         <div className="name-row">
           <div className="input-group">
             <label>First name:</label>
-            <input type="text" name="first-name" />
+            <input type="text" name="first-name" onChange={this.handleChange} required />
           </div>
           <div className="input-group">
             <label>Last name:</label>
-            <input type="text" name="last-name" />
+            <input type="text" name="last-name" onChange={this.handleChange} required />
           </div>
         </div>
         <div className="input-group">
+          <label>Email:</label>
+          <input type="email" name="email" onChange={this.handleChange} required />
+        </div>
+        <div className="input-group">
           <label>Subject:</label>
-          <input type="text" name="subject" />
+          <input type="text" name="subject" onChange={this.handleChange} required />
         </div>
         <div className="input-group">
           <label>Message:</label>
-          <textarea rows="6" name="subject" />
+          <textarea rows="6" name="subject" onChange={this.handleChange} required />
         </div>
-        <input type="hidden" name="bot-field" />
         <div className="button-container">
           <Button type="submit">Send message</Button>
         </div>
